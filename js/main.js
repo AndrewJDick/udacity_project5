9c64c77dfc3cf95512157009d12d23668d00aae4
Andrew.Dick <andrew.dick@cohaesus.co.uk>
2016-03-08 21:59:54 +0000

    main.js linted

--- js/main.js
+++ js/main.js
@@ -1,43 +1,45 @@
+"use strict";
+
 /* ======= Model ======= */
 
 var model = {
 
-	locations: [
+    locations: [
         {
-            name : 'Cohaesus Projects Ltd',
+            name: 'Cohaesus Projects Ltd',
             lat: '51.524288',
-            lng: '-0.096178', 
+            lng: '-0.096178',
             about: 'The office. You work here.',
-            url: 'http://www.cohaesus.co.uk'
+            visible: true
         },
         {
-            name : 'The Fountain',
+            name: 'The Fountain',
             lat: '51.526938',
-            lng: '-0.088713', 
+            lng: '-0.088713',
             about: 'Local pub. Good beer selection.',
-            url: 'http://www.cohaesus.co.uk'
+            visible: true
         },
         {
-            name : 'Old Street Market',
+            name: 'Old Street Market',
             lat: '51.523658',
-            lng: '-0.093210', 
+            lng: '-0.093210',
             about: 'Great place for lunch.',
-            url: 'http://www.cohaesus.co.uk'
+            visible: true
         },
         {
-            name : 'Finsbury Leisure Centre',
+            name: 'Finsbury Leisure Centre',
             lat: '51.526212',
-            lng: '-0.095253', 
+            lng: '-0.095253',
             about: 'Local gym / fitness centre.',
-            url: 'http://www.cohaesus.co.uk'
+            visible: true
         },
         {
-            name : 'Vibast Community Centre',
+            name: 'Vibast Community Centre',
             lat: '51.525467',
-            lng: '-0.092290', 
+            lng: '-0.092290',
             about: 'Conducts Salah',
-            url: 'http://www.cohaesus.co.uk'
-        },
+            visible: true
+        }
     ]
 };
 
@@ -47,107 +49,122 @@ var model = {
 
 var viewModel = {
 
-	init: function() {
+    init: function () {
 
-		// Tell our views to initialise
-		markerView.init();
-		mapView.init();
-	},
+        var self = this;
 
-	// Retrieves the locations object, containing all hard-coded markers
-	getMarkers: function() {
-		return model.locations;
+        var locations = model.locations;
+
+        // Stores our model objects in an observable array
+        this.items = ko.observableArray(locations);
+
+        // Track the user input of the search box (empty by default)
+        this.currentSearch = ko.observable('');
+
+        function matchesSearch(poi) {
+            // console.log(poi);
+            // Return anything that exists in the name or about properties of the list view that matches the search.
+            var currentSearch = self.currentSearch();
+            return poi.name.indexOf(currentSearch) >= 0 || poi.about.indexOf(currentSearch) >= 0;
+        }
+
+        // Since currentSearch is initially blank, the function will return all objects in the model, generating an <li><a><p> for each of them.
+        this.filteredItems = ko.computed(function () {
+            return self.items().filter(matchesSearch);
+        });
+
+        // a combined observable that will trigger its subscribers whenever either the search OR the items are updated
+        this.searchAndItems = ko.computed(function () {
+            return {
+                currentSearch: self.currentSearch(),
+                items: self.items().map(function (poi) {
+                    return Object.assign({}, poi, {
+                        visible: matchesSearch(poi)
+                    });
+                })
+            };
+        });
+
+        // Tell our views to initialise
+        markerView.init();
+        mapView.init();
     },
+
+    // Retrieves the locations object, containing all hard-coded markers
+    getMarkers: function () {
+        return model.locations;
+    }
 };
 
-
+ko.applyBindings(viewModel.init);
 
 
 /* ======= View ======= */
 
 var markerView = {
 
-	init: function() {
+    init: function () {
         // store the DOM element for easy access later
         this.pointOfInterest = document.getElementById('poi-list');
 
         // render this view (update the DOM elements with the right values)
         this.render();
-    },
-
-	render: function() {
-        var poi;
-        
-        // get the marker locations we'll be rendering from the viewModel
-        var poi = viewModel.getMarkers();
-
-        function filter(locations) {
-			var self = this;
-
-	    	this.items = ko.observableArray(locations);
-	    	// Track the user input of the search box (empty by default)
-	    	this.currentSearch = ko.observable('');
-
-	    	// Since currentSearch is initially blank, the function will return all objects in the model, generating an <li><a><p> for each of them.
-	    	this.filteredItems = ko.computed(function() {
-	      	
-	      		return self.items().filter(function(item) {
-
-	        		return item.name.indexOf(self.currentSearch()) >= 0, item.about.indexOf(self.currentSearch()) >= 0;
-	      		});
-    		});
-    	}
-
-    	ko.applyBindings(new filter(poi));
     }
 };
 
 
-
 var mapView = {
 
-	init: function() {
-		// Store the DOM element for later use
-		this.mapElem = document.getElementById('map');
+    init: function () {
+        // Store the DOM element for later use
+        this.mapElem = document.getElementById('map');
 
-		// render the map view
-		this.render();
-	},
+        // render the map view
+        this.render();
+    },
 
-	render: function() {
+    render: function () {
 
-		var marker;
+        // Store the context of this for our forEach loop
+        var self = this;
 
-		// Store the context of this for our forEach loop
-		var self = this;
+        // Retrieves map marker co-ords from the ViewModel
+        var poi = viewModel.getMarkers();
 
-		// Retrieves map marker co-ords from the ViewModel
-		var poi = viewModel.getMarkers();
+        this.mapOldSt = {
+            center: new google.maps.LatLng(51.524288, -0.096178),
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-		this.mapOldSt = {
-    		center: new google.maps.LatLng(51.524288,-0.096178),
-    		zoom: 16,
-    		mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
+        // Generates the map of Old Street
+        this.mapElem = new google.maps.Map(document.getElementById('map'), this.mapOldSt);
 
-		// Generates the map of Old Street
-		this.mapElem = new google.maps.Map(document.getElementById('map'), this.mapOldSt);
+        poi.forEach(function (location) {
 
-		poi.forEach(function(location) {
-			var markerOptions = {
-		     	position: new google.maps.LatLng(location.lat,location.lng)
-			};	
+            // viewModel.searchAndItems.subscribe(function(searchAndItems) {
+            //     console.log(searchAndItems);
+            //     // in here, searchAndItems.currentSearch === currentSearch
+            //      // and searchAndItems.items === items with `visible` set properly
+            // })
 
-			var marker = new google.maps.Marker(markerOptions);
-			marker.setMap(self.mapElem);
+            var markerOptions = {
+                position: new google.maps.LatLng(location.lat, location.lng)
+            };
 
-			google.maps.event.addListener(marker,'click',function(e) {
-				var markerInfoOptions = { content: location.about };
-				var markerInfo = new google.maps.InfoWindow(markerInfoOptions);
-				markerInfo.open(self.mapElem, marker);
-  			});
-		});	
-	}
+            var marker = new google.maps.Marker(markerOptions);
+            
+            // Displays the marker if it appears in the search listings.
+            marker.setMap(self.mapElem);
+
+            // Adds an infowindow above the location when the marker is clicked
+            google.maps.event.addListener(marker, 'click', function () {
+                var markerInfoOptions = {content: location.about};
+                var markerInfo = new google.maps.InfoWindow(markerInfoOptions);
+                markerInfo.open(self.mapElem, marker);
+            });
+        });
+    }
 };
 
 
