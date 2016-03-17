3db6d0cd80dc9170ea02dba0c2e6866d5299f339
Andrew.Dick <andrew.dick@cohaesus.co.uk>
2016-03-17 13:22:17 +0000

    prepping for 3rd party API

--- src/js/main.js
+++ src/js/main.js
@@ -78,7 +78,7 @@ var viewModel = {
         });
 
         // Tell our views to initialise
-        markerView.init();
+        listView.init();
         mapView.init();
 
         // Data is populated afterwards to trigger the filteredItems computed function for the first time.
@@ -89,15 +89,37 @@ var viewModel = {
 
 /* ======= View ======= */
 
-var markerView = {
+var listView = {
 
     init: function () {
         // store the DOM element for easy access later
         this.pointOfInterest = document.getElementById('poi-list');
+
+        // render the map view
+        this.render();
     },
 
     render: function () {
 
+        this.listClick = function(location) {
+
+            // Searches for a specified property in an array's elements. 
+            function findMarker(array, attr, value) {
+                for(var i = 0; i < array.length; i += 1) {
+                    if(array[i][attr] === value) {
+                        return i;
+                    }
+                }
+            }
+
+            // This will loop through the markers array and try to match location.name with the name property of markers elements (set in mapView.mapMarkers).  
+            var locateMapMarker = findMarker(mapView.markers, 'name', location.name);
+
+            // Add the infoWindow and bounce animation to the respective map marker when the list <h4> is clicked.
+            var marker = mapView.markers[locateMapMarker];
+            mapView.toggleBounce(marker);
+            mapView.infoWindow(location, marker);
+        };
     }    
 };
 
@@ -128,7 +150,7 @@ var mapView = {
         this.mapElem = new google.maps.Map(document.getElementById('map'), this.mapOldSt);
 
         // Items stored in this array will display as a marker on the OldSt map.
-        var markers = [];
+        this.markers = [];
 
         // The markers are now subscribed to the viewModel.filterItems computed observable.
         // Whenever the user makes any changes to currentSearch, the previous markers will be cleared and replaced with the new values of viewModel.items.
@@ -136,15 +158,16 @@ var mapView = {
         viewModel.filteredItems.subscribe(function (poi) {
 
             // Remove existing markers from the OldSt map.
-            markers.forEach(function (marker) {
+            self.markers.forEach(function (marker) {
                 marker.setMap(null);
             });
 
             // Add new markers.
             // The map() method creates a new array with the results of calling a provided function on every element in the poi array.
-            markers = poi.map(self.plotMarkers); 
+            self.markers = poi.map(self.plotMarkers); 
         });
 
+        // Creates a marker for each item in viewModel.items
         this.mapMarkers = function (location) {
             var markerOptions = {
                 animation: google.maps.Animation.DROP,
@@ -153,11 +176,13 @@ var mapView = {
 
             var marker = new google.maps.Marker(markerOptions);
 
+            // the name property is added so that the list view can calculate which marker to display the infoWindow above. 
             marker.name = location.name;
 
             return marker;
         };
 
+        // Plots the generated markers on the Old St map
         this.plotMarkers = function(location) {
             
             var marker = self.mapMarkers(location);
@@ -174,6 +199,7 @@ var mapView = {
             return marker;
         };
 
+        // Makes the selected map marker bounce when it is selected in either the map or list view. 
         this.toggleBounce = function(marker) {
             if (marker.getAnimation() !== null) {
                 marker.setAnimation(null);
@@ -182,30 +208,12 @@ var mapView = {
             }
         };
 
+        // Creates an infoWindow above the map marker
         this.infoWindow = function(location, marker) {
             var markerInfoOptions = {content: location.about};
             var markerInfo = new google.maps.InfoWindow(markerInfoOptions);
             markerInfo.open(self.mapElem, marker);
         };
-
-        this.listClick = function(location) {
-
-            function findMarker(array, attr, value) {
-                for(var i = 0; i < array.length; i += 1) {
-                    if(array[i][attr] === value) {
-                        return i;
-                    }
-                }
-            }
-
-            // This will loop through the markers array and try to match location.name with the name property of markers elements (set in mapView.mapMarkers).  
-            var locateMapMarker = findMarker(markers, 'name', location.name);
-
-            // Add the infoWindow and bounce animation to the respective map marker when the list <h4> is clicked.
-            var marker = markers[locateMapMarker];
-            self.toggleBounce(marker);
-            self.infoWindow(location, marker);
-        }; 
     }
 };
 
